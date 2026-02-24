11c82e1750b4dbad9660f22e9f08b25664b43aee
Omar <omarramadan021@gmail.com>
2026-02-24 03:03:42 +0200

    Custom drawe done

---
+++ lib/widgets/drawer_items_list_view.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/models/drawer_item_model.dart';
+import 'package:responsive_dashboard/utils/app_images.dart';
+import 'package:responsive_dashboard/widgets/drawer_item.dart';
+
+class DrawerItemsListView extends StatefulWidget {
+  const DrawerItemsListView({super.key});
+
+  @override
+  State<DrawerItemsListView> createState() => _DrawerItemsListViewState();
+}
+
+class _DrawerItemsListViewState extends State<DrawerItemsListView> {
+  int activeIndex = 0;
+  final List<DrawerItemModel> drawerItems = [
+    DrawerItemModel(image: Assets.imagesDashboard, title: "Dashboard"),
+    DrawerItemModel(
+      image: Assets.imagesMyTransctions,
+      title: "My Transactions",
+    ),
+    DrawerItemModel(image: Assets.imagesStatistics, title: "Statistics"),
+    DrawerItemModel(image: Assets.imagesWalletAccount, title: "Wallet Account"),
+    DrawerItemModel(image: Assets.imagesMyInvestments, title: "My Investments"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList.builder(
+      itemCount: drawerItems.length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          onTap: () {
+            if (activeIndex != index) {
+              setState(() {
+                activeIndex = index;
+              });
+            }
+          },
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: DrawerItem(
+              drawerItemModel: drawerItems[index],
+              isActive: activeIndex == index,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
