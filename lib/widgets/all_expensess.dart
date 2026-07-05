890416b2d8965dbebd2cf2c36560582681df1edc
Omar <omarramadan021@gmail.com>
2026-07-05 19:45:05 +0300

    Refactor AllExpensess widget to use CustomBackgroundContainer for improved layout consistency

--- lib/widgets/all_expensess.dart
+++ lib/widgets/all_expensess.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/widgets/all_expensess_header.dart';
 import 'package:responsive_dashboard/widgets/all_expensess_item_list.dart';
+import 'package:responsive_dashboard/widgets/custom_background_container.dart';
 
 class AllExpensess extends StatelessWidget {
   const AllExpensess({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      decoration: ShapeDecoration(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-        color: Colors.white,
-      ),
+    return CustomBackgroundContainer(
+      padding: 20 ,
       child: Column(
         children: [
           AllExpensessHeader(),
@@ -23,3 +20,4 @@ class AllExpensess extends StatelessWidget {
     );
   }
 }
+
